24a47f6a9737a7aa50d9ad68b77b05ea560ebaa9
NagarajMurgod <nagarajmurgod27oct@gmail.com>
2025-04-19 18:45:04 +0530

    build model

--- ChatApp/app/models.py
+++ ChatApp/app/models.py
@@ -18,5 +18,17 @@ class CustomUser(AbstractUser):
 class Chats(TimeStampedModel):
     group_name = models.CharField(max_length=100)
     group_id = models.UUIDField(default=uuid.uuid4,unique=True)
-    
+    members = models.ManyToManyField(User, related_name='chats')
+    is_group = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f"{self.group_name}_{self.group_id}"
+
+class Messages(TimeStampedModel):
+    chat = models.ForeignKey(Chats,related_name='messages')
+    sender = models.ForeignKey(User)
+    content = models.CharField(max_length=2000)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.sender}_{self.chat}"
